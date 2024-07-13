2789babff520d7a3def77673700a6c2e0dfffc9e
rhuan580 <rhuan580@gmail.com>
2024-07-12 21:29:50 -0300

    Update problema_reuso_codigo.js

---
+++ problema_reuso_codigo.js
@@ -0,0 +1,43 @@
+// 1 - Passar  em algoritmo a fórmula para calcular a área de um círculo.
+// 1:. Criar uma constante chamada pi e atribuir o valor 3.14
+// 2:. Criar uma constante chamada raio e atribuir o valor 5
+// 3:. Criar uma constante chamada area e atribuir pi * (raio * raio)
+
+// 2 - De última hora conversei com o cliente e descobri que necessito reutilizar essa fórmula em outros trechos do programa/software.
+
+
+// Função
+
+    function calcularAreaCirculo(raio){
+        const pi = 3.14;
+        return area = pi * (raio * raio);
+    }
+
+console.log(calcularAreaCirculo(10));
+
+// 3 - Foi solicitado para que você faça o mesmo procedimento feito com a fórmula de área só que dessa vez você vai fazer com a área do triângulo
+// 1:. Pesquisar a fórmula da área do triângulo
+// 2:. Definir a uma função chamada calcularAreatriangulo
+// 3:. colocar um console.log(calcularAreaTriangulo(10))
+// ÁREA DO TRIÂNGULO 
+
+function calcularAreaTriangulo(base, altura){
+return area = (base * altura) / 2;
+}
+console.log(calcularAreaTriangulo(10,30));
+
+// const base = 10;
+// const altura = 30;
+// return area = (base * altura) / 2;
+
+// 3 - Pesquisar e procurar mais uma forma geométrica da escolha de vocês, para inserir no software de matemática de formas geométricas.
+
+// ÁREA DO QUADRADO
+
+function calcularAreaQuadrado(lado){
+    return area = lado * lado; 
+}
+
+console.log(calcularAreaQuadrado(30));
+
+
